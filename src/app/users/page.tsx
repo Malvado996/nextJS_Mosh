71a84990ac747a32bef8fd43c39223483c06a82b
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2026-01-11 20:06:00 -0500

    caching

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,23 +1,33 @@
-
-
 const UsersPage = async () => {
-
     interface User {
         id: number;
         name: string;
     }
 
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+        {
+            revalidate: 10,  // top-level works fine
+        } as RequestInit & { revalidate?: number }  // quick assertion
+    );
+
+    // Optional: handle errors
+    if (!res.ok) {
+        throw new Error('Failed to fetch users');
+    }
+
     const users: User[] = await res.json();
 
     return (
         <>
             <h1>Ajaxs Minions</h1>
             <ul>
-                {users.map(user => <li key={user.id}>{user.name}</li>)}
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
             </ul>
         </>
-    )
-}
+    );
+};
 
-export default UsersPage
+export default UsersPage;
